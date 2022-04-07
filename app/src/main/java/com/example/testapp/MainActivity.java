e1bd25b5c4ac437554ba2bc0de40ba528f747ba0
a18fayha <a18fayha@student.his.se>
2022-04-07 09:57:12 +0200

    Template for mountain adapter

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> berg = Arrays.asList("Kinnekulle", "klimanjaro", "Billengen", "K2", "Hanneberg");
+
+        recyclerView = findViewById(R.id.testRecyclerView);
+
     }
 }
